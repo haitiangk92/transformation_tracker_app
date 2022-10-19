0d477bacc9db0d069afe63958e7aef4130d22e38
haitiankg92 <haitiankg92@gmail.com>
2022-10-19 14:35:50 -0400

    new db connector

--- db_connector.py
+++ db_connector.py
@@ -1,20 +1,30 @@
-import psycopg2 as pg
+# import psycopg2 as pg
 
-conn = pg.connect(
-    database="personal_fitness",
-    user="postgres",
-    password="4mathft",
-    host="thernizetech.hopto.org",
-    port="5432"
-)
+# conn = pg.connect(
+#     database="personal_fitness",
+#     user="postgres",
+#     password="4mathft",
+#     host="thernizetech.hopto.org",
+#     port="5432"
+# )
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
 # cur.execute("INSERT INTO raw_data.users VALUES (%s,%s,%s,%s,%s)",
 #             (1, "haitiangk92@gmail.co", "Emmanuel", "Thernize", "test_password"))
 # conn.commit()
 
-cur.execute(
-    "SELECT column_name, data_type FROM information_schema.columns where table_name = 'users'")
+# cur.execute(
+#     "SELECT column_name, data_type FROM information_schema.columns where table_name = 'users'")
 
-print(cur.fetchall())
+# print(cur.fetchall())
+
+import requests as r
+
+api_url = "http://thernizetech.hopto.org"
+
+req = r.get(api_url)
+
+res = req.text
+
+print(res)
